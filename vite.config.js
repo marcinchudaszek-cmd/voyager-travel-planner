ca37381b747c763a3e022503c10535ebe960a8b8
Marcin <marcinchudaszek@example.com>
2026-04-27 20:07:50 +0200

    fix: base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/voyager-travel-planner/',  // ← zmień na nazwę repo na GitHub
+  base: '/',  // ← zmień na nazwę repo na GitHub
   build: {
     outDir: 'dist',
     sourcemap: false
